6492ab957e36eae8bb60fc79f7693c158fb7b36a
omkarpatilkit <41175678+omkarpatilkit@users.noreply.github.com>
2022-06-08 16:45:19 +0000

    array basics

--- array/ArrayBasics.java
+++ array/ArrayBasics.java
@@ -3,13 +3,15 @@ package array;
 public class ArrayBasics {
 
     public static void main(String[] args) {
-        int a[] = { 1, 2, 3, 7, 8, 9 };
+        int a[] = { 11, 1, 2, 3, 7, 8, 9 };
         int key = 3;
         printArray(a);
         Boolean isThere = isPresent(a, key);
         if (isThere)
             System.out.println("present");
         deleteFirstOcc(a, 7);
+        System.out.println(maxPosition(a));
+        System.out.println(secondLargest(a));
     }
 
     static Boolean isPresent(int a[], int key) {
@@ -41,11 +43,45 @@ public class ArrayBasics {
         int pos = isPresentInt(a, key);
         if (pos == -1)
             return;
+        else if (pos == a.length - 1) {
+            a[a.length - 1] = 0;
+            return;
+        }
 
         for (int i = pos; i < a.length - 1; i++) {
             a[i] = a[i + 1];
         }
         a[a.length - 1] = 0;
         printArray(a);
+    }
+
+    static int maxPosition(int a[]) {
+        int max = 0;
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > a[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static int secondLargest(int a[]) {
+
+        int max = 0;
+        int secMax = -1;
+        for (int i = 1; i < a.length; i++) {
+
+            if (a[i] > a[max]) {
+
+                secMax = max;
+                max = i;
+            } else if (a[i] < a[max]) {
+                if (secMax == -1 || a[i] > a[secMax]) {
+                    secMax = i;
+                }
+            }
+        }
+        return secMax;
     }
 }
